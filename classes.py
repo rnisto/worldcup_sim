47a64ad702ce95c7a76c98a4cd03af34b3fa3861
rnisto <rs.dykes@icloud.com>
2026-06-09 11:56:14 +0100

    adding simulate game functions

--- classes.py
+++ classes.py
@@ -1,3 +1,9 @@
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import pandas as pd
+from scipy.stats import poisson
+import numpy as np
+
 class Team:
     """An object to store information on international teams"""
     def __init__ (self, name, attack, defence):
@@ -7,13 +13,41 @@ class Team:
 
 class Match:
     """An object to store information about games"""
-    def __init__(self, date, competition, home_team, away_team, 
-                 home_goals, away_goals, neutral, result):
+    def __init__(self, date, competition, home_team, away_team):
         self.date = date
         self.competition = competition
         self.home_team = home_team
         self.away_team = away_team
-        self.home_goals = home_goals
-        self.away_goals = away_goals
-        self.neutral = neutral
-        self.result = result
+
+    def predicted_goals(self, model):
+        home_avg = model.predict(
+            pd.DataFrame({
+                "team": [self.home_team],
+                "opponent": [self.away_team],
+                "home": [0]
+            })
+        ).iloc[0]
+
+        away_avg = model.predict(
+            pd.DataFrame({
+                "team": [self.away_team],
+                "opponent": [self.home_team],
+                "home": [0]
+            })
+        ).iloc[0]
+
+        return home_avg, away_avg
+
+    def simulate_result(self, model):
+        home_avg, away_avg = self.predicted_goals(model)
+
+        home_goals = np.random.poisson(home_avg)
+        away_goals = np.random.poisson(away_avg)
+
+        return home_goals, away_goals
+    
+    def simulate_shootout(self):
+        winner = np.random.choice([self.home_team, self.away_team])
+
+        return winner
+
--- run.py
+++ run.py
@@ -3,14 +3,16 @@ import numpy as np
 import datetime as datetime
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
+from scipy.stats import poisson
+import classes
 
 data = pd.read_csv("https://raw.githubusercontent.com/martj42/international_results/refs/heads/master/results.csv", on_bad_lines='warn')
 data["date"] = pd.to_datetime(data["date"], format='%Y-%m-%d')
 
-# data["days_since"] = (datetime.datetime.today() - data["date"]).dt.days
-# data["time_weight"] = np.exp(data["days_since"] * -0.0018)
-# lookback_period = datetime.timedelta(days = 365 * 5)
-# data = data[(data["days_since"] < 365*5) & (data["days_since"] > 0)]
+data["days_since"] = (datetime.datetime.today() - data["date"]).dt.days
+data["time_weight"] = np.exp(data["days_since"] * -0.0018)
+lookback_period = datetime.timedelta(days = 365 * 5)
+data = data[(data["days_since"] < 365*5) & (data["days_since"] > 0)]
 
 # filtering teams with limited games
 data = data.groupby('home_team').filter(lambda x: len(x) >= 10)
